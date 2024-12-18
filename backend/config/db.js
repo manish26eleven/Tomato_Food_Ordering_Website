35fb03ce7bfa14f16e262a0845d107d08ae33ba4
manish26eleven <118613667+manish26eleven@users.noreply.github.com>
2024-12-18 11:01:18 +0530

    Add files via upload

---
+++ backend/config/db.js
@@ -0,0 +1,5 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+  // await mongoose.connect('mongodb+srv://manish26eleven:manish2611@clustermine.3ta0i.mongodb.net/foodapp').then(()=> console.log("DB Connected"));
+}
