c298e9de4d4579cdd4e36f187b1fba29d1fbcebf
manish26eleven <118613667+manish26eleven@users.noreply.github.com>
2024-12-18 10:58:03 +0530

    Add files via upload

---
+++ Admin/src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,27 @@
+import { NavLink } from 'react-router-dom'
+import { assets } from '../../assets/assets'
+import './Sidebar.css'
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <div className='sidebar'>
+      <div className="sidebar-options">
+        <NavLink to='/add' className="sidebar-option">
+          <img src={assets.add_icon}/>
+          <p>Add Items</p>
+        </NavLink>
+        <NavLink to='/list' className="sidebar-option">
+          <img src={assets.order_icon}/>
+          <p>List Items</p>
+        </NavLink>
+        <NavLink to='/orders' className="sidebar-option">
+          <img src={assets.order_icon}/>
+          <p>Orders</p>
+        </NavLink>
+      </div>
+      </div>
+  )
+}
+
+export default Sidebar
